1901b340efcc50a01d228d534425d8d5b8b8fe1f
sivamani <98547578+sivamaniPITTALA@users.noreply.github.com>
2024-06-06 18:36:47 +0530

    Time: 65 ms (40.94%), Space: 33.2 MB (16.04%) - LeetHub

---
+++ 0876-hand-of-straights/0876-hand-of-straights.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+    if (hand.size() % groupSize != 0) {
+        return false; // The total number of cards must be divisible by the group size
+    }
+
+    map<int, int> count;
+    for (int card : hand) {
+        count[card]++;
+    }
+
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    for (const auto& entry : count) {
+        minHeap.push(entry.first);
+    }
+
+    while (!minHeap.empty()) {
+        int first = minHeap.top();
+        for (int i = 0; i < groupSize; i++) {
+            if (count[first + i] > 0) {
+                count[first + i]--;
+                if (count[first + i] == 0) {
+                    if (minHeap.top() == first + i) {
+                        minHeap.pop();
+                    }
+                }
+            } else {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+};
